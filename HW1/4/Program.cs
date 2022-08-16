903c8acf2939910680790c0cefabc691ffecf0c4
Максим Тагаев <tagaew.m@gmail.com>
2022-08-16 04:15:58 +0300

    Fix 4

--- HW1/4/Program.cs
+++ HW1/4/Program.cs
@@ -2,14 +2,12 @@
 int NumberA = Convert.ToInt32(Console.ReadLine());
 int n = 2;
 
-if (NumberA % 2 == 0)
-while (n <= NumberA)
+if (NumberA < 2)
 {
-    Console.Write(n + " ");
-    n = n + 2;
+    Console.Write("Введите число >= 2");
 }
-else
-while (n < NumberA)
+
+while (n <= NumberA)
 {
     Console.Write(n + " ");
     n = n + 2;
