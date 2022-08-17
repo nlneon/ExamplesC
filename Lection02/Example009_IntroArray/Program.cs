982de43a08960ec9d46b1f59a6aa4fd54542ec95
Максим Тагаев <tagaew.m@gmail.com>
2022-08-17 23:44:28 +0300

    Lection02

---
+++ Lection02/Example009_IntroArray/Program.cs
@@ -0,0 +1,73 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int max = arg1;
+    if (arg2 > max)
+    {
+        max = arg2;
+    }
+    if (arg3 > max)
+    {
+        max = arg3;
+    }
+    return max;
+}
+
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+// array[0] = 12; - замена значения
+
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 142, b2 = 23, c2 = 33;
+int a3 = 13, b3 =23, c3 = 33;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int unrealMax = Max(max1, max2, max3);
+
+int unrealMax2 = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine($"Максимальное число: {unrealMax}");
+Console.WriteLine($"Максимальное число: {unrealMax2}");
+
+int max = a1;
+if (b1 > max)
+{
+    max = b1;
+}
+if (c1 > max)
+{
+    max = c1;
+}
+if (a2 > max)
+{
+    max = a2;
+}
+if (b2 > max)
+{
+    max = b2;
+}
+if (c2 > max)
+{
+    max = c2;
+}
+if (a3 > max)
+{
+    max = a3;
+}
+if (b3 > max)
+{
+    max = b3;
+}
+if (c3 > max)
+{
+    max = c3;
+}
+
+Console.WriteLine($"Максимальное число: {max}");
---
+++ Lection02/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collectiion)
+{
+    int lenght = collectiion.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collectiion[index] = new Random().Next(1,10);
+        
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf( int[] cillection, int find)
+{
+    int count = cillection.Length;
+    int index = 0;
+    int position = -1; // чтобы понятно было, когда элемента нет
+    while (index < count)
+    {
+        if(cillection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10]; // массив из 10 чисел, заполненный нулями
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
