4f205c6983938a07c73f6e687deb6cb31abd8a2b
Максим Тагаев <tagaew.m@gmail.com>
2022-08-30 18:46:21 +0300

    fix

--- HW6/2/Program.cs
+++ HW6/2/Program.cs
@@ -16,9 +16,16 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 
 void FindPoint(double b1, double k1, double b2, double k2)
 {
-    double x = (b2 - b1) / (k1 - k2);
-    double y = k1 * x + b1;
-    Console.WriteLine($"Координаты точки пересечения прямых ({x}; {y})");
+    if (k1 == k2)
+    {
+        Console.WriteLine("Данные прямые не пересекаются");
+    }
+    else if (k1 != k2)
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Координаты точки пересечения прямых ({x};{y})");
+    }
 }
 
 FindPoint(b1, k1, b2, k2);
