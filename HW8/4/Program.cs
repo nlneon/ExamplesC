6f9254e7a02984bbf619bb6963529820c476678e
Максим Тагаев <tagaew.m@gmail.com>
2022-09-06 03:37:40 +0300

    new MakeArray

--- HW8/4/Program.cs
+++ HW8/4/Program.cs
@@ -7,73 +7,43 @@
 // 26(1,0,1) 55(1,1,1)
 
 
-int[,,] array = new int[2, 2, 2];
-
-int[,,] MakeArray(int[,,] array)
-{
-    int first = 10;
-    for (int k = 0; k < array.GetLength(2); k++)
-    {
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i,j,k] = first;
-                first++;
-            }
-        }
-    }
-    return array;
-}
-
-// + реализация рандомного массива с просторов интернета)
-static IEnumerable<int> UniqueRandomNumbers(int from, int to)
-{
-    Random rand = new();
-    int[] values = Enumerable.Range(from, to).ToArray();
-    int i = values.Length;
-    while (i > 0)
-    {
-        int j = rand.Next(i--);
-        yield return values[j];
- 
-        (values[i], values[j]) = (values[j], values[i]);
-    }
-}
-
 int[,,] arr = new int[2, 2, 2];
- 
-IEnumerator<int> iterator = UniqueRandomNumbers(10, 99).GetEnumerator();
- 
-for (int i = 0; i < arr.GetLength(0); i++)
-for (int j = 0; j < arr.GetLength(1); j++)
-for (int k = 0; k < arr.GetLength(2); k++)
+
+Random r = new Random();
+
+int[,,] MakeArray(int[,,] arr)
 {
-    if (!iterator.MoveNext())
-    {
-        Console.WriteLine("Упс, уникальные числа закончились!");
-        return;
-    }
- 
-    arr[i, j, k] = iterator.Current;
+   var numbers = Enumerable.Range(10, 99).ToList();
+   for (int i = 0; i < arr.GetLength(0); i++)
+   {
+       for (int j = 0; j < arr.GetLength(1); j++)
+       {
+           for (int k = 0; k < arr.GetLength(2); k++)
+           {
+               var randomIndex = r.Next(0, numbers.Count);
+               var number = numbers[randomIndex];
+               numbers.RemoveAt(randomIndex);
+               arr[i, j, k] = number;
+           }
+       }
+   }
+   return arr;
 }
 
-void PrintArray(int[,,] array)
+void PrintArray(int[,,] arr)
 {
-    for (int k = 0; k < array.GetLength(2); k++)
+    for (int k = 0; k < arr.GetLength(2); k++)
     {
-        for (int i = 0; i < array.GetLength(0); i++)
+        for (int i = 0; i < arr.GetLength(0); i++)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
+            for (int j = 0; j < arr.GetLength(1); j++)
             {
-                Console.Write($"{array[i,j,k]}({i},{j},{k})\t");
+                Console.Write($"{arr[i,j,k]}({i},{j},{k})\t");
             }
             Console.WriteLine();
         }
     }
 }
 
-MakeArray(array);
-PrintArray(array);
-Console.WriteLine();
+MakeArray(arr);
 PrintArray(arr);
